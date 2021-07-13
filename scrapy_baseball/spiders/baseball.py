dc8b2815b1a83d07a208a4bf26b9576aeb7f04a4
johkakka <johkakka.rits@gmail.com>
2021-07-13 20:57:42 +0900

    今日はここまで

--- scrapy_baseball/spiders/baseball.py
+++ scrapy_baseball/spiders/baseball.py
@@ -8,6 +8,11 @@ class BaseballSpider(scrapy.Spider):
     allowed_domains = ['baseball.yahoo.co.jp']
     start_urls = ['https://baseball.yahoo.co.jp/npb/game/2021000214/score?index=0110100']
 
+    def start_requests(self):
+        for i in range(100, 300):
+            yield scrapy.Request('https://baseball.yahoo.co.jp/npb/game/2021000'+str(i)+'/score?index=0110100',
+                                 callback=self.parse)
+
     def parse(self, response):
         result = []
         pitcher = response.css('div#pit .nm a::text').extract_first()
