d85b76b33868290b36ab4ffe2f190d51b164f2db
johkakka <johkakka.rits@gmail.com>
2021-07-13 15:59:57 +0900

    出場投手を取得＆打席結果を取得

--- scrapy_baseball/spiders/baseball.py
+++ scrapy_baseball/spiders/baseball.py
@@ -14,6 +14,14 @@ class BaseballSpider(scrapy.Spider):
 
         for raw in raws:
             player = raw.css('td.bb-statsTable__data--player a::text').extract_first()
+            res = [False]*9
+            battings_raw = raw.css('td.bb-statsTable__data--inning')
+            for i in range(len(battings_raw)):
+                r = battings_raw[i].css('div.bb-statsTable__dataDetail').extract_first()
+                if r is not None:
+                    res[i] = True
+            print(res)
+
 
             if player is None:
                 no = 0
@@ -28,3 +36,27 @@ class BaseballSpider(scrapy.Spider):
             no += 1
 
         print(result_list)
+
+        pitcher_list = []
+        tables = response.css('table.bb-scoreTable')
+        team = True
+        for table in tables:
+            raws = table.css('tr.bb-scoreTable__row')
+            no = 0
+
+            for raw in raws:
+                player = raw.css('td.bb-scoreTable__data--player a::text').extract_first()
+                ip = raw.css('td.bb-scoreTable__data--score p::text').extract()[1]
+
+                pitcher_list.append({
+                    'player': player,
+                    'no': no,
+                    'team': team,
+                    'ip': ip
+                })
+                no += 1
+
+            no = 0
+            team = False
+
+        print(pitcher_list)
