0597a50e002918aa8ce5d4528c2884d84a5c4634
johkakka <johkakka.rits@gmail.com>
2021-07-13 20:00:22 +0900

    一打席の結果を取得

--- scrapy_baseball/spiders/baseball.py
+++ scrapy_baseball/spiders/baseball.py
@@ -7,8 +7,8 @@ class BaseballSpider(scrapy.Spider):
     start_urls = ['https://baseball.yahoo.co.jp/npb/game/2021000214/score?index=0110100']
 
     def parse(self, response):
-        result_list = []
 
+        pitcher = response.css('div#pit .nm a::text').extract_first()
+        batter = response.css('div#batter .nm a::text').extract_first()
 
-
-        print(result_list)
+        print(pitcher+","+batter)
