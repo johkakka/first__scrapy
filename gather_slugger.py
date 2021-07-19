e2e77334145ca03daf21d4087ba25c1f5f52f08e
johkakka <johkakka.rits@gmail.com>
2021-07-19 20:14:30 +0900

    対戦成績の2次元表を作成

--- gather_slugger.py
+++ gather_slugger.py
@@ -1,10 +1,32 @@
 path = 'baseball_100_627.csv'
 
 def main():
+    dic = {}
     with open(path) as f:
         _ = f.readline()
         for line in f:
-            print(line)
+            batter, pitcher, res = line.rstrip().split(",")
+            res = int(res)
+
+            # box = 0
+            bat = 0
+            hit = 0
+            if res > 0:
+                bat += 1
+            if res > 1:
+                hit += 1
+
+            if batter not in dic:
+                dic[batter] = {pitcher: [bat, hit]}
+            else:
+                b = dic[batter]
+                if pitcher not in b:
+                    b[pitcher] = [bat, hit]
+                else:
+                    b[pitcher][0] += bat
+                    b[pitcher][1] += hit
+
+    print(dic)
 
 if __name__ == '__main__':
     main()
